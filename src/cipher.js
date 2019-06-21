706e5e3d3c00d140bbc4f08f166070efb7fa3bad
natyjimenez <naty.jimenez@gmail.com>
2019-06-21 01:34:08 -0400

    avance

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,47 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
-  },
-  decode: () => {
-    /* Acá va tu código */
+  
+     encode: (myString, offset1) => {
+  
+      let bigLetter = myString.toUpperCase();//Mayúsculas   
+      let myStringCipher = ''; // almacena string Cifrado 
+      for (let i = 0 ; i < bigLetter.length ; i++){ // recorre el mensaje   
+      let numberLetter= bigLetter.charCodeAt(i);// numero de letra en codigo ASCII   
+      if (numberLetter >= 65 && numberLetter <= 90) { // letras mayúsculas en ASCII  
+      let cipherFormula = ( numberLetter - 65 + offset1) % 26 + 65; // formula de Cifrado Cesar   
+      let newLetter =  String.fromCharCode(cipherFormula);// Valor de la letra cifrada   
+      myStringCipher +=newLetter;// Formar string cifrado 
+        }
+      else if(numberLetter === 32){ // Retorna el valor de la cadena cifrada 
+      myStringCipher += ' ';// Añadir espacio       
+        }
+      else { // Detener si no es Letra
+           break;      
+        }
+        }
+      return myStringCipher;// Resultado String cifrado   
+       
+      },
+  
+    decode: (myString1, offset2) => {
+  
+      let bigLetter1 = myString1.toUpperCase(); //Mayúsculas 
+      let myStringDecipher1 = ''; // almacena string Cifrado 
+      for (let i = 0 ; i < bigLetter1.length ; i++){ // recorre el mensaje     
+      let numberLetter1= bigLetter1.charCodeAt(i); // numero de letra en codigo ASCII   
+      if (numberLetter1 >= 65 && numberLetter1 <= 90) { // letras mayúsculas en ASCII    
+      let decipherFormula1 = ( numberLetter1 + 65 - offset2) % 26 + 65;// formula de Cifrado Cesar       
+      let newLetter1 =  String.fromCharCode(decipherFormula1);// Valor de la letra cifrada   
+      myStringDecipher1 +=newLetter1;// Formar string cifrado 
+         }
+      else if(numberLetter1 === 32){ // Retorna el valor de la cadena cifrada  
+      myStringDecipher1 += ' ';// Añadir espacio       
+         }
+      else {// Detener si no es Letra
+      break;      
+         }
+         }
+      return myStringDecipher1;// Resultado String cifrado   
+       
+      
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 /*Acá va tu código*/
+
+//Mostrar Resultado en Index
+const btnSave = document.getElementById('btnCodifica');
+btnSave.addEventListener('click', () => {
+let myString = document.getElementById('textoIngresado').value;
+let offset1 = parseInt(document.getElementById('inpOffset').value);
+let myStringCipher=window.cipher.encode(myString,offset1);
+document.getElementById('resultado').value =myStringCipher;
+ })
+
+ 
+//Mostrar Resultado en Index
+const btnSave1 = document.getElementById('btnDeco');
+btnSave1.addEventListener('click', () => {
+let myString1 = document.getElementById('textoIngresado').value;
+let offset2 = parseInt(document.getElementById('inpOffset').value);
+let myStringDecipher=window.cipher.decode(myString1,offset2);
+document.getElementById('resultado').value =myStringDecipher;
+ })
